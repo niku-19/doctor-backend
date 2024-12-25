2f8e7fece397718acbdc246d64a64b62cc38e29a
niku-19 <nikhil@Nikhils-MacBook-Air.local>
2024-12-26 00:30:44 +0530

    first

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+    await mongoose.connect("mongodb+srv://nikhil:nikhil@cluster0.hl6q2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 }
 
